cb4f9063782319ba6213ba7cfc609a4e16341bdc
yuri0427a <yuri0427ita@gmail.com>
2020-03-12 10:09:50 +0000

    [Finshed]応用課題３

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get '/home/about' => 'homes#about' 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    post '/relationships' => 'relationships#create'
+    delete '/relationships' => 'relationships#destroy'
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+
   resources :books
   resources :book, only: [:new, :create, :index, :show] do
     resource :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
-end
+  end
+
+  
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_064714) do
+ActiveRecord::Schema.define(version: 2020_03_11_051201) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2020_03_10_064714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "book_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
